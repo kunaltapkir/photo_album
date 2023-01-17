6c6d3862b449bf733c6bb97be1841ff98c7f2796
kunaltapkir <kunaltapkir75@gmail.com>
2023-01-17 18:06:20 +0530

    delete user added.

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,4 +1,5 @@
 import className from 'classnames';
+import { GoSync } from 'react-icons/go';
 
 function Button({
   children,
@@ -9,12 +10,14 @@ function Button({
   danger,
   outline,
   rounded,
+  loading,
   ...rest
 }) {
   const classes = className(
     rest.className,
-    'flex items-center px-3 py-1.5 border',
+    'flex items-center px-3 py-1.5 border h-8',
     {
+      'opacity-80': loading,
       'border-blue-500 bg-blue-500 text-white': primary,
       'border-gray-900 bg-gray-900 text-white': secondary,
       'border-green-500 bg-green-500 text-white': success,
@@ -26,13 +29,13 @@ function Button({
       'text-gray-900': outline && secondary,
       'text-green-500': outline && success,
       'text-yellow-400': outline && warning,
-      'text-red-500': outline && danger,
+      'text-red-500': outline && danger
     }
   );
 
   return (
-    <button {...rest} className={classes}>
-      {children}
+    <button {...rest} disabled={loading} className={classes}>
+      {loading ? <GoSync className="animate-spin" /> : children}
     </button>
   );
 }
@@ -51,7 +54,7 @@ Button.propTypes = {
         'Only one of primary, secondary, success, warning, danger can be true'
       );
     }
-  },
+  }
 };
 
 export default Button;
--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,46 +1,49 @@
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { fetchUsers, addUser } from '../store';
+import { useThunk } from '../hooks/use-thunk';
 import Button from './Button';
 import Skeleton from './Skeleton';
+import UsersListItem from './UsersListItem';
 
 function UsersList() {
-  const dispatch = useDispatch();
-  const { isLoading, data, error } = useSelector((state) => state.users);
+  const [doFetchUsers, isLoadingUsers, loadingUsersError] =
+    useThunk(fetchUsers);
+
+  const [doCreateUser, isCreatingUser, creatingUserError] = useThunk(addUser);
+
+  const { data } = useSelector((state) => state.users);
 
   useEffect(() => {
-    dispatch(fetchUsers());
-  }, [dispatch]);
+    doFetchUsers();
+  }, [doFetchUsers]);
 
   const handleUserAdd = () => {
-    dispatch(addUser());
+    doCreateUser();
   };
 
-  if (isLoading) {
-    return <Skeleton times={6} className="h-10 w-full" />;
-  }
+  let content;
 
-  if (error) {
-    return <div>Error fetching data...</div>;
+  if (isLoadingUsers) {
+    content = <Skeleton times={6} className="h-10 w-full" />;
+  } else if (loadingUsersError) {
+    content = <div>Error fetching data...</div>;
+  } else {
+    content = data.map((user) => {
+      return <UsersListItem key={user.id} user={user} />;
+    });
   }
-
-  const renderedUsers = data.map((user) => {
-    return (
-      <div key={user.id} className="mb-2 border rounded">
-        <div className="flex p-2 justify-between items-center cursor-pointer">
-          {user.name}
-        </div>
-      </div>
-    );
-  });
 
   return (
     <div>
-      <div className="flex flex-row justify-between m-3">
+      <div className="flex flex-row justify-between items-center m-3">
         <h1 className="m-2 text-xl">Users</h1>
-        <Button onClick={handleUserAdd}>+ Add User</Button>
+        <Button loading={isCreatingUser} onClick={handleUserAdd}>
+          + Add User
+        </Button>
+        {creatingUserError && 'Error creating user...'}
       </div>
-      {renderedUsers}
+      {content}
     </div>
   );
 }
