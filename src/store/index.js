7ec8d787c6ab0010d7424a5d8eba85188a6ed8e3
kunaltapkir <kunaltapkir75@gmail.com>
2023-01-18 12:25:35 +0530

    rtk query added for adding and displaying albums for particular user.

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,22 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/dist/query';
 import { usersReducer } from './slices/usersSlice';
+import { albumsApi } from './apis/albumsApi';
 
 const store = configureStore({
   reducer: {
-    users: usersReducer
+    users: usersReducer,
+    [albumsApi.reducerPath]: albumsApi.reducer
+  },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(albumsApi.middleware);
   }
 });
+
+setupListeners(store.dispatch);
 
 export { store };
 export * from './thunks/fetchUsers';
 export * from './thunks/addUser';
 export * from './thunks/removeUser';
+export { useFetchAlbumsQuery, useAddAlbumMutation } from './apis/albumsApi';
