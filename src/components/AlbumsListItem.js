0ceb003f9e7621618f8a91445d9c6088c8acd2cf
kunaltapkir <kunaltapkir75@gmail.com>
2023-01-18 15:49:20 +0530

    delete album completed.

---
+++ src/components/AlbumsListItem.js
@@ -0,0 +1,33 @@
+import Button from './Button';
+import { useRemoveAlbumMutation } from '../store';
+import ExpandablePanel from './ExpandablePanel';
+import { GoTrashcan } from 'react-icons/go';
+
+function AlbumsListItem({ album }) {
+  const [removeAlbum, results] = useRemoveAlbumMutation();
+
+  const handleRemoveAlbum = () => {
+    removeAlbum(album);
+  };
+
+  const header = (
+    <>
+      <Button
+        className="mr-2"
+        loading={results.isLoading}
+        onClick={handleRemoveAlbum}
+      >
+        <GoTrashcan />
+      </Button>
+      {album.title}
+    </>
+  );
+
+  return (
+    <ExpandablePanel key={album.id} header={header}>
+      List of photos in the album
+    </ExpandablePanel>
+  );
+}
+
+export default AlbumsListItem;
